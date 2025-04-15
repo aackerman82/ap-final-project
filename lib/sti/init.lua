0f14487090a9fd1760899b60153adab27ac768b2
Sean Lauritzen <seanl@desktop.lauritzenwifi.net>
2025-04-15 19:18:17 -0400

    STI library hack for repeating backgrounds

--- lib/sti/init.lua
+++ lib/sti/init.lua
@@ -1092,8 +1092,8 @@ function Map:drawImageLayer(layer)
 			end
 		-- if we're repeating on X alone...
 		elseif layer.repeatx then
-			local x = imagewidth
-			while x < self.width * self.tilewidth do 
+			local x = imagewidth * -100
+			while x < self.width * self.tilewidth * 100 do 
 				lg.draw(layer.image, x, layer.y)
 				x = x + imagewidth
 			end
