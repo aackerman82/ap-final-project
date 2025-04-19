5a9a3515df68149ef605042115d05bf5d36b3b58
aackerman82 <ackman1217@gmail.com>
2025-04-19 11:54:45 -0400

    Currently bugged
    Working on understanding and getting the object's image file properly - that's what's currently broken (def is nil / it's not getting the full img path from the objects in the castle.lua file)

--- src/gameworld.lua
+++ src/gameworld.lua
@@ -10,10 +10,25 @@ function GameWorld:initialize()
     self.coins = 0
     self.lives = 5 -- Lives != health
     self.isPaused = false
+    --just for testing - this'll be changed so we can change maps
+    self.map = sti("assets/levels/castle.lua")
     self.cameraPos = {
         x = 0,
         y = 0
     }
+    entities = {
+        enemies = {},
+        collectables = {}
+    }
+    for _, object in pairs(self.map.objects) do
+        if object.type == "Enemy" then
+            local enemy = Enemy:new(object)
+            table.insert(self.entities.enemies, enemy)
+        elseif object.type == "Collectable" then
+            local collectable = Collectable:new(object)
+            table.insert(self.entities.collectables, collectable)
+        end
+    end
 end
 
 function GameWorld:tostring()
