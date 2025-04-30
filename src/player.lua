73fcdd2ab14212c71fcb9501913ac7391929a6ab
aackerman82 <ackman1217@gmail.com>
2025-04-30 13:00:49 -0400

    Lowered default sfx volume and a few additions

--- src/player.lua
+++ src/player.lua
@@ -5,6 +5,8 @@ Player = middleclass.class("Player", Character)
 
 function Player:initialize(object)
     Character.initialize(self, object)
+    self.health = 3
+    self.money = 0
 end
 
 function Player:update(dt)
@@ -19,9 +21,11 @@ function Player:update(dt)
 	end
     
     if self.grounded then  -- If on the ground
-        if love.keyboard.isDown("space") then
+        if love.keyboard.isDown("space", "kp0") then --kp0 because I'm an arrow keys user -Angela
             sound = love.audio.newSource("assets/sound/jump.wav", "static")
             sound:setPitch(1 + math.random() / 6)
+            --adjust for your own ears
+            sound:setVolume(0.1)
             love.audio.play(sound)
             self.y_vel = -300
         end
